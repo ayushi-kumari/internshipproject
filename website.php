a0634bf0fef4dc9f64ea89284675902b0084b45f
ayushi-kumari <65759506+ayushi-kumari@users.noreply.github.com>
2021-06-14 16:24:36 +0530

    first commit

---
+++ website.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>free-bird</title>
+  <link rel="icon" type="image" href="https://th.bing.com/th/id/OIP.bOnIvrXzrlgccwEUrTU-eAHaHa?w=199&h=199&c=7&o=5&dpr=1.5&pid=1.7">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style>	
+
+#h{
+	word-spacing: 2px;
+}
+.q{
+	margin-left: 40px;
+	margin-right: 20px;
+  color:#931D5A ;
+}
+#y{
+	color:#E6EEFF;
+  font-weight: bold;
+  font-family:Brush Script MT;
+  font-size: 25px;
+
+}
+.nav{
+	margin-top: -3%;
+	margin-left: 10%;
+  
+  color: #E6EEFF;
+  padding-bottom: 10px;
+  padding-top:-7px;
+  text-align:justify;
+
+}
+.nav:hover{
+  color: navy;
+
+
+}
+.c{
+	background-color:#931D5A;
+}
+#w{
+	margin-right: 50%;
+	color: #1A6093;
+	
+	padding-top: 20px;
+	
+}
+.s{
+	margin-left: 59%;
+	margin-top: -17%;
+}
+#p{
+	color:#1A6093;
+	font-size: 17px;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+td, th {
+  border: 1px solid black;
+  text-align: left;
+  padding: 8px;
+  color: black;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+.button{
+  font-family: arial, sans-serif;
+  background-color: #62529c;
+        border: none;
+        color: #fff;
+        padding: 15px 30px;
+        text-decoration: none;
+        margin: 4px 2px;
+        cursor: pointer;
+
+ }
+ .material-icons{
+  padding: 20px;
+  font-size: 30px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  color:white;
+  border-radius: 100%;
+}
+
+.material-icons:hover {
+    opacity: 0.7;
+}  
+.last{
+  margin-left: 60%;
+  
+} 
+.l{
+    height:90px;
+    width:1245px;
+    font-size: 40px;
+      animation:example 3s infinite;
+      
+   
+}
+@keyframes example{
+  0%{
+    color: pink;
+      background-color: skyblue;
+    }
+  50%{color: white;
+      background-color: grey;
+    }
+  100%{
+    color: navy;
+    background-color:palevioletred;
+  }
+}
+    .row{
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-left: 55%;
+        margin-top: -55%;
+      }
+
+
+.col{
+  flex-basis: 50%;
+  min-width: 650px;
+}
+
+.small{
+  display:  flex;
+  background-color:#1A6093;
+  margin: 20px 0;
+  align-items: center;
+  border-radius: 6px;
+  overflow: hidden;
+  width: 85%;
+}
+   .sm{
+    poition: relative;
+
+
+   }
+  p{
+    margin-left: 20px;
+    color: lightseagreen;
+    line-height: 22px;
+    font-size: 15px;
+    font-size: 18px;
+
+   }
+
+   .iframe{
+    width: 70px;
+   }
+
+ .new{
+  width: 50%;
+ }
+ .roll{
+  background-color:#ffe4e1;
+
+ }
+ .love{
+  background-color: black;
+  height: 450px;
+
+ }
+ #e{
+  font-size: 30px;
+ }
+ #ff{
+  margin-right: 800px;
+  font-size: 18px;
+  width: 30%;
+  word-spacing: 7px;
+ }
+ .yes{
+  margin-left: 110%;
+  margin-bottom: 90%;
+  margin-top: -34%;
+ }
+ #dd{
+  margin-left: 700px;
+  font-size: 18px;
+  word-spacing: 7px;
+  margin-top: -37%;
+  width: 33%;
+
+ }
+ .no{
+margin-right:1%;
+margin-left:100%;
+margin-top: -37%;
+ }
+ .deep{
+  background-color:#f7fdf0;
+  margin-top: -20px;
+ }
+ 
+
+
+</style>
+
+</head>
+<body id="h">
+<div class="c">
+<img height="50px" width="60px" 
+ src="https://th.bing.com/th/id/OIP.bOnIvrXzrlgccwEUrTU-eAHaHa?w=199&h=199&c=7&o=5&dpr=1.5&pid=1.7">
+<div class="nav">
+  <i id="y"> FREE-BIRD</i>
+  <a href="fbd.php" class="nav">FBD TEST</a>
+  <a href="fields.php" class="nav">FIELDS</a>
+  <a href="contact.php" class="nav">CONTACT US</a>
+  <a href="passwrd.php" class="nav">REGISTER</a>
+  <a href="loginsql.php" class="nav">LOGIN</a>
+</div>
+</div>
+
+<img  height="350px" width ="1247px"src="https://abaforlawstudents.com/wp-content/uploads/2016/03/internships.jpg">
+<div class="roll">
+<marquee></h3> <em> IMPORTANT DATES-</em>  Internship Registration starts from-25 June ------> last date to apply- 28 July ...HURRY !</h3></marquee>
+
+<div class="q">
+<center><h2>ABOUT US</h2></center>
+<h3 id="w">
+	FIRE-BIRD is a self-financed project to provide quality education to anyone interested in learning from the IITs. Every January and July, anywhere between number of internships are offered online - free of cost - for anyone to enroll and learn from.We also provide free certification to the eligible candidates all over INDIA.Interested candidates can go through the guidelines.</h3>
+<div class="s">
+<img  height="250px" width="500px"src="http://photos.demandstudios.com/getty/article/244/85/200249342-001_XS.jpg">
+</div><br><br>
+</div>
+</div>
+<div class="deep">
+<h3>ELEGIBILITY AND SELECTION</h3>
+<ul id="p">
+<h4><li>FIRE-BIRD office will contact the course instructor to check if they can take in interns from toppers of their respective courses.</li>
+<li>Depending on the availability and affirmation from the course instructor, the course toppers are contacted requesting them to apply for internship if interested.</li>
+<li>The documents are sent to the Course instructor who decides on whether the candidate is eligible for the internship.
+Course instructor decision is final in all matters with respect to selection of candidates.</li></h4></ul>
+<h3> DURATION OF INTERNSHIP</h3>
+<ul>
+<h4 id="p"><li>FIRE-BIRD will be offering 2, 4, 6 or 8 weeks internship. This is purely based on the candidate’s and course instructor’s availability and interest.</li>
+<li>Such internships are offered once in summer (May/June/July) and winter (Nov/Dec/Jan) each year.</h4></ul></li>
+<div class="new">
+<h3>APPLICATION</h3>
+<h4 id="p">The candidate should apply for the FBD TEST for their desired internship.Details for the same are provided below.
+<ul>
+<li>Copy of your Resume</li>
+<li>Your current Image & Signature</li>
+</ul>
+The application is forwarded to the course instructor by FREE-BIRD.<br><br>
+Once approval is obtained, the candidate will be contacted.</h4>
+<h3> ACCOMODATION</h3>
+<h4 id="p"> FREE-BIRD will try to arrange for your accommodation at one of the hostels within the institute premises. In case of non-availability, the candidate has to arrange for his/her own accommodation.</h4>
+
+<table>
+  <tr>
+    <th>Duration</th>
+    <th>Stipend per month</th>
+  </tr>
+  <tr>
+    <td>2 weeks</td>
+    <td>2500</td>
+  </tr>
+  <tr>
+    <td>4 weeks</td>
+    <td>5000</td>
+  </tr>
+  <tr>
+    <td>6 weeks</td>
+    <td>7500</td>
+  </tr>
+  <tr>
+    <td>8 weeks</td>
+    <td>10,000</td>
+  </tr>
+</table>
+<br><br><br><br><br>
+</div>
+<center>
+<form method ="post" action="loginsql.php">
+<input type="submit" name="apply now" value="APPLY NOW" class=button>
+</form>
+</center>
+</div>
+</div>
+</div>
+<br><br>
+<div class="row">
+<div class="col">
+<div class="small">
+  <div class="sm">
+    <iframe src="https://player.vimeo.com/external/371566920.sd.mp4?s=61ec26851fb1de9fd96c8315c8f7698f48aed371&profile_id=139&oauth2_token_id=57447761"></iframe>
+  </div>
+  <p>Videos related to our website</p>
+</div> 
+<div class="small">
+  <div class="sm">
+    <iframe src="https://player.vimeo.com/external/210752478.sd.mp4?s=cd769c55093473d7c9480780603dec110389d26f&profile_id=164&oauth2_token_id=57447761"></iframe>
+  </div>
+  <p>Our co- ordinators in our FREE-BIRD</p>
+</div> 
+<div class="small">
+  <div class="sm">
+    <iframe src="https://player.vimeo.com/external/371837723.sd.mp4?s=7a4920e3315f22617298c9f072d1f11f03843be2&profile_id=139&oauth2_token_id=57447761"></iframe>
+  </div>
+  <p>Our new creaations in this field</p>
+</div> 
+</div>
+</div>
+<br>
+<br>
+<br>
+<br><br><br><br>
+<div class="love">
+  <br><br>
+  <center><p id="e">People loved FREE-BIRD.com.....</p><br></center>
+  <p id="ff">"The organisation provides full time/part time work from home Opportunity and even a WFH internship. There is absolute flexibility with time and no pressure as such. The company expects you to deliver results within certain timelines, which is good".
+  <img  height="100" width="100" src="https://th.bing.com/th/id/OIP.zVuxCOwxfCIPDfeJtWKSKgHaHa?pid=ImgDet&rs=1" class="yes"></p>
+<p id="dd">"Very good work environment. Friendly work culture. I learned many things while working here. Everyday, they provide opportunity to develop. They also organize extra curricular activities with working."
+<img height="100" width="100"  src="https://th.bing.com/th/id/R6dacd274b0224abd36b63d97d1da72f5?rik=a88H6gCez8RtzA&riu=http%3a%2f%2fkylerthomas.wpmulti.wfgtitleco.com%2fwp-content%2fuploads%2fsites%2f338%2f2020%2f10%2fKyler-Thomas.png&ehk=1gTo419ep9NYaXyS70vaFFPw0RvITfAG3GcI4Vj8dIk%3d&risl=&pid=ImgRaw"class="no">
+</div>
+<div class="l">
+  <center> FREE-BIRD </center>
+<br>
+<div class="last">
+<a href="fb.php" class="material-icons" style="font-size:24px;color:navy;">facebook</a>
+<a href="shareit.php" class="material-icons"style="font-size:24px;color:black;">share</a>
+<a href="contact.php" class="material-icons"style="font-size:24px;color:#1A6093;">mail</a>
+<a href="contact.php" class="material-icons"style="font-size:24px;color:black;">call</a>
+<a href="website.php" class="material-icons"style="font-size:24px;color:black;">verified</a>
+
+</div>
+</div>
+
+<p>Themes Features Blog Stats VIP Terms of Service Privacy Policy
+<a href="login.php"style="color:lightseagreen;font-size: 20px;">My admin</a>
+</p>
+</body>
+
+</html>
